768419908a0d0f80015e7c68b7e324f6e604d33b
liudaoan <Liudaoan@xunlei.com>
2016-07-18 16:51:41 +0800

    修改了界面

--- app/src/main/java/com/example/testgit/MainActivity.java
+++ app/src/main/java/com/example/testgit/MainActivity.java
@@ -9,9 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        int a = 0;
-        a = a + 1;
-        a = 4;
+        String a = "";
     }
 }
